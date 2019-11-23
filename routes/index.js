518e13e83d31b306e07979fcde7a9b219dd06117
Vitaliy Vatulya <vitaliy.vatulya@namecheap.com>
2019-11-23 23:23:30 +0200

    WS test tool

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,6 @@ router.get('/test', function routeTest(req, res) {
   res.send('test');
 });
 
+router.get('/wsTest', require('./wsTest'));
+
 module.exports = router;
--- wsEvents/index.js
+++ wsEvents/index.js
@@ -4,4 +4,10 @@ module.exports = function wsEvents (socket) {
       console.log('WS disconnected');
     });
 
+    socket.on('message', function wsMessageHandler (data) {
+      console.log('WS message');
+      console.log(data);
+      socket.emit('message', data);
+    })
+
 };
