0c3c3e9116cfe53bce43966404fdb19b25aa89c9
Vitaliy Vatulya <vitaliy.vatulya@namecheap.com>
2019-11-26 23:05:43 +0200

    Added config baseHost key and ws.uri

--- config/development.js
+++ config/development.js
@@ -1,6 +1,10 @@
+const urljoin = require('url-join');
+const defer = require('config/defer').deferConfig;
+
 module.exports = {
   basePath: '/',
+  baseHost: 'http://127.0.0.1',
   ws: {
-    uri: '//',
+    uri: defer((cfg) => urljoin(cfg.baseHost, cfg.basePath)),
   },
 };
--- config/production.js
+++ config/production.js
@@ -1,6 +1,10 @@
+const urljoin = require('url-join');
+const defer = require('config/defer').deferConfig;
+
 module.exports = {
   basePath: '/http_connector',
+  baseHost: process.env.APP_HOST,
   ws: {
-    uri: '//http_connector',
+    uri: defer((cfg) => urljoin(cfg.baseHost, cfg.basePath)),
   },
 };
